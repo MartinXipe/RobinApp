d4156275ad136d6cdbdad592b618c6a2aca42013
Luistllz <luistllz97@gmail.com>
2020-12-29 11:51:23 -0700

    IOS

--- ExampleLink/ExampleLink.iOS/LoginPageRenderer.cs
+++ ExampleLink/ExampleLink.iOS/LoginPageRenderer.cs
@@ -1,6 +1,8 @@
 ﻿using ExampleLink.iOS;
+using ExampleLink.Model;
 using ExampleLink.Views;
 using Foundation;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,22 +21,38 @@ namespace ExampleLink.iOS
         {
             base.ViewDidAppear(animated);
 
+
             var auth = new OAuth2Authenticator(
-                clientId: "86mir46493vare", // your OAuth2 client id
-                scope: "", // the scopes for the particular API you're accessing, delimited by "+" symbols
-                authorizeUrl: new Uri("https://www.linkedin.com/uas/oauth2/authorization"),
-                redirectUrl: new Uri("https://www.Google.com"));
+                clientId: "86mir46493vare",
+                clientSecret: "AdN49aqfrfv6JIgV",
+                scope: "r_liteprofile",
+                authorizeUrl: new Uri("https://www.linkedin.com/oauth/v2/authorization"),
+                redirectUrl: new Uri("https://www.google.com/"),
+                accessTokenUrl: new Uri("https://www.linkedin.com/oauth/v2/accessToken")
+                );
 
-            auth.Completed += async (sender, eventArgs) => {
-                // We presented the UI, so it's up to us to dimiss it on iOS.
-                //App.SuccessfulLoginAction.Invoke();
-
+            auth.Completed += async (sender, eventArgs) =>
+            {
                 if (eventArgs.IsAuthenticated)
                 {
-                    // Use eventArgs.Account to do wonderful things
-                    //App.SaveToken(eventArgs.Account.Properties["access_token"]);
-                    
+                    string Token = eventArgs.Account.Properties["access_token"];
+                    var resq = new OAuth2Request(
+                                              "GET",
+                                              new Uri("https://api.linkedin.com/v2/me?"
+                                              + "oauth2_access_token="
+                                              + Token
+                                              + "&projection=(firstName,lastName,profilePicture(displayImage~:playableStreams))"),
+                                              null,
+                                              eventArgs.Account
+                                              );
 
+                    LinkModel linkModel = new LinkModel();
+                    resq.AccessTokenParameterName = "oauth2_access_token";
+                    var LinkResponse = await resq.GetResponseAsync();
+                    var json = LinkResponse.GetResponseText();
+                    linkModel = JsonConvert.DeserializeObject<LinkModel>(json);
+                    Xamarin.Forms.Application.Current.Properties["Login"] = linkModel;
+                    await Xamarin.Forms.Application.Current.SavePropertiesAsync();
 
                 }
                 else
